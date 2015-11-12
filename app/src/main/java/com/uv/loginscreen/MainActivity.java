8a786864897549d917ff1ed92137385063f7eeba
umaparvathy <umavenkee@gmail.com>
2015-11-12 23:12:13 +0530

    Intents

--- app/src/main/java/com/uv/loginscreen/MainActivity.java
+++ app/src/main/java/com/uv/loginscreen/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uv.loginscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -31,19 +32,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                boolean emailHasError = false, passwordHasError = false;
                 if(email.getText().length()==0)
                 {
+                    emailHasError = true;
                     email.setError("Field cannot be left blank.");
                 } else {
+                    emailHasError = false;
                     email.setError(null);
                 }
 
                 if(password.getText().length()==0)
                 {
+                    passwordHasError = true;
                     password.setError("Field cannot be left blank.");
                 } else {
+                    passwordHasError = false;
                     password.setError(null);
                 }
+                if(!emailHasError && !passwordHasError) {
+                    Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                    intent.putExtra("userName", email.getText().toString());
+                    startActivity(intent);
+                }
+
             }
         });
     }
